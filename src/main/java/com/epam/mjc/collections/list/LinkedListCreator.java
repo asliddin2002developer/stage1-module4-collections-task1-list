fb9f474168747e6713879dd31625c1b564a2aee2
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-12 09:44:07 +0500

    List exercises

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> resultLinkedList = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 0){
+                resultLinkedList.addLast(integer);
+            }else{
+                resultLinkedList.addFirst(integer);
+            }
+        }
+        return resultLinkedList;
     }
+
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
+    public int asecndingOrder(String n) {
+        int nInt = Integer.parseInt(n);
+        return (int)Math.pow(5 * nInt, 2) + 3;
+    }
+
     @Override
     public int compare(String a, String b) {
+        if (asecndingOrder(a) == asecndingOrder(b)) {
+            return a.compareTo(b);
+        } else {
+            return asecndingOrder(a) - asecndingOrder(b);
+        }
     }
 }
